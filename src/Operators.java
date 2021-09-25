a763230c7462c8c72da1843750120cfd36fe2b8e
Niranjana <niranjanamishra374@gmail.com>
2021-09-25 15:33:06 -0600

    Initial commit

---
+++ src/Operators.java
@@ -0,0 +1,22 @@
+public class Operators {
+    public static void main(String [] args){
+        int a=10;
+        int b=20;
+        System.out.println("sum of a and b is;"+(a+b));
+        System.out.println("mul of a and b is;"+(a*b));
+        System.out.println("div of a and b is="+(a/b));
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a!=b);
+         boolean x=true;
+         boolean y=false;
+         System.out.println(x&&y);
+         System.out.println(!x);
+         a=10; a++;
+         System.out.println(a);
+         b=20;
+         b--;
+         System.out.println(b);
+         
+    }
+}
---
+++ src/java.java
@@ -0,0 +1,8 @@
+public class java {
+    public static void main (String [] args)
+    {
+        System.out.println("My Name Is Niranjana");
+        System.out.println( "This is my first project");
+    }
+
+}
